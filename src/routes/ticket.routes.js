7abd2b4f6d57d18aa854c1da691c0f0fb2231125
moataz-saad <mtzsd329@gmail.com>
2026-04-14 02:39:47 +0200

    add missing newline at end of ticket.routes.js

--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const ticketController = require("../controllers/ticket.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 const adminMiddleware = require("../middleware/admin.middleware");
+
 router.get("/:bookingId", authMiddleware, ticketController.generateTicket);
 router.post('/verify-checkin', authMiddleware, adminMiddleware, ticketController.verifyAndCheckIn);
 module.exports = router;
