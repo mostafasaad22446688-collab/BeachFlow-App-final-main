56dc2f8146109894a2e2b6f29bf1797ef05c8a32
moataz-saad <mtzsd329@gmail.com>
2026-04-15 05:06:06 +0200

    fix: remove duplicate roleStatus field in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,9 +4,9 @@ const { sequelize } = require("../config/db");
 module.exports = sequelize.define("User", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, allowNull: false },
+  roleStatus: {type: DataTypes.ENUM('none', 'pending', 'approved', 'rejected'),defaultValue: 'none'},
   email: { type: DataTypes.STRING, unique: true, allowNull: false },
   password: { type: DataTypes.STRING, allowNull: false },
-  roleStatus: {type: DataTypes.ENUM('none', 'pending', 'approved', 'rejected'),defaultValue: 'none'},
   otp: { type: DataTypes.STRING },
   profilePic: {type: DataTypes.STRING,allowNull: true,defaultValue: "https://example.com/default-avatar.png"},
   isVerified: { type: DataTypes.BOOLEAN, defaultValue: false },
