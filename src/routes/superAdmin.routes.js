eda56667acc330247235fc78e1fc78f5ce28bea2
moataz-saad <mtzsd329@gmail.com>
2026-04-17 14:23:07 +0200

    refactor: add spacing and console

--- src/routes/superAdmin.routes.js
+++ src/routes/superAdmin.routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+
+
+
 const isSuperAdmin = require("../middleware/superAdmin.middleware");
 const authMiddleware = require("../middleware/auth.middleware");
 const superAdminController = require("../controllers/superAdmin.controller");
+
+console.log("Auth:", authMiddleware);
+console.log("SuperAdmin:", isSuperAdmin);
+console.log("Controller Func:", superAdminController.getPendingAdmins);
 
 router.get("/super-requests", authMiddleware, isSuperAdmin, superAdminController.getPendingAdmins);
 router.post("/super-action", authMiddleware, isSuperAdmin, superAdminController.handleAdminAction);
