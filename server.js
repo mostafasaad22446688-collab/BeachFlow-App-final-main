a7a5d9001c2f0d1f8295b3f3fd0b899f4a8a7509
moataz-saad <mtzsd329@gmail.com>
2026-07-11 15:59:08 +0300

    refactor: streamline database connection and sync logic in server.js; add vercel configuration

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
+
 const { connectDB, sequelize } = require("./src/config/db"); 
 require('dotenv').config(); 
 const app = require("./app"); 
 
-
 const start = async () => {
-  await connectDB();
-sequelize.sync()
-  .then(() => {
+  try {
+    await connectDB();
+    await sequelize.sync();
     console.log("✅ Database updated Successfully!");
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`🚀 Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error(" SQL Sync Error Details:");
+  } catch (err) {
+    console.error("❌ SQL Sync or Connection Error Details:");
     console.error(err); 
     process.exit(1); 
-  });
+  }
 };
+
 start();
 
+module.exports = app;
 
